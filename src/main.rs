0338173786a93cd2c50c629f700e8e14ad0f813c
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-10-11 10:40:54 +0200

    Extract entry display into a trait
    This allows me to have other implementations of entry displays and plug
    them in. :-)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::path::{Path,PathBuf};
 use std::fmt;
+use std::slice::Iter;
 
 struct Entry {
     path: PathBuf,
@@ -42,21 +43,8 @@ impl Entry {
         }
     }
 
-    fn size(&self) -> u64 {
-        self.self_size + self.descendent_size()
-    }
-
     fn descendent_size(&self) -> u64 {
         self.children.iter().map(|child| child.size()).fold(0, |a, n| a + n)
-    }
-
-    fn file_name(&self) -> &str {
-        // Converting paths to strings might fail. We start by trying to convert the
-        // filename. If the path is something like ".", it will fail as there is no "file
-        // name" in that path. We fall back to the entire path in that case. If that also
-        // fails, we fall back to hardcoded representation.
-        let file_name = self.path.file_name().and_then(|s| s.to_str());
-        file_name.or_else(|| self.path.to_str()).unwrap_or("(no name)")
     }
 }
 
@@ -87,24 +75,49 @@ impl SizeDisplay for u64 {
     }
 }
 
-impl fmt::Display for Entry {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.file_name(), self.size().as_size_display())
+trait DisplayableEntry : fmt::Display + Sized {
+    fn size(&self) -> u64;
+    fn name(&self) -> &str;
+    fn children_iter(&self) -> Iter<Self>;
+}
+
+impl DisplayableEntry for Entry {
+    fn size(&self) -> u64 {
+        self.self_size + self.descendent_size()
+    }
+
+    fn name(&self) -> &str {
+        // Converting paths to strings might fail. We start by trying to convert the
+        // filename. If the path is something like ".", it will fail as there is no "file
+        // name" in that path. We fall back to the entire path in that case. If that also
+        // fails, we fall back to hardcoded representation.
+        let file_name = self.path.file_name().and_then(|s| s.to_str());
+        file_name.or_else(|| self.path.to_str()).unwrap_or("(no name)")
+    }
+
+    fn children_iter(&self) -> Iter<Entry> {
+        self.children.iter()
     }
 }
 
-fn print_tree(dir_entry: Entry) {
-    print_indented_tree(dir_entry, 0);
+impl fmt::Display for Entry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.name(), self.size().as_size_display())
+    }
 }
 
-fn print_indented_tree(entry: Entry, indent: usize) {
+fn print_tree<T: DisplayableEntry>(entry: &T) {
+    print_indented_tree(entry, 0);
+}
+
+fn print_indented_tree<T: DisplayableEntry>(entry: &T, indent: usize) {
     println!("{0:1$}{2}", "", indent * 2, entry);
-    for child in entry.children {
+    for child in entry.children_iter() {
         print_indented_tree(child, indent + 1);
     }
 }
 
 fn main() {
     let root = Entry::for_path(PathBuf::from(".")).unwrap();
-    print_tree(root);
+    print_tree(&root);
 }
