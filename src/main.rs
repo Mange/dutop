0f4851b6aece8e33311a75cb8abcad220dd37133
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-10-12 22:38:10 +0200

    Move the byte display logic to utils

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use std::slice::Iter;
 mod arguments;
 mod utils;
 
+use utils::SizeDisplay;
+
 struct Entry {
     path: PathBuf,
     self_size: u64,
@@ -51,33 +53,6 @@ impl Entry {
 
     fn descendent_size(&self) -> u64 {
         self.children.iter().map(|child| child.size()).fold(0, |a, n| a + n)
-    }
-}
-
-trait SizeDisplay {
-    fn as_size_display(&self) -> String;
-}
-
-const KILO: f64 = 1e3 as f64;
-const MEGA: f64 = 1e6 as f64;
-const GIGA: f64 = 1e9 as f64;
-
-const KILO_CUTOFF: u64 = (0.6 * KILO) as u64;
-const MEGA_CUTOFF: u64 = (1.4 * MEGA) as u64;
-const GIGA_CUTOFF: u64 = (1.4 * GIGA) as u64;
-
-impl SizeDisplay for u64 {
-    fn as_size_display(&self) -> String {
-        if *self < KILO_CUTOFF {
-            return format!("{} B", *self)
-        }
-
-        let (scaled, unit) = match *self {
-            KILO_CUTOFF...MEGA_CUTOFF => (*self as f64 / KILO, "kB"),
-            MEGA_CUTOFF...GIGA_CUTOFF => (*self as f64 / MEGA, "MB"),
-            _ => (*self as f64 / GIGA, "GB"),
-        };
-        format!("{:.2} {}", scaled, unit)
     }
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,32 @@ pub fn describe_io_error(error: Error) -> String {
         _ => "Unrecognized error"
     }.to_string()
 }
+
+pub trait SizeDisplay {
+    fn as_size_display(&self) -> String;
+}
+
+const KILO: f64 = 1e3 as f64;
+const MEGA: f64 = 1e6 as f64;
+const GIGA: f64 = 1e9 as f64;
+
+const KILO_CUTOFF: u64 = (0.6 * KILO) as u64;
+const MEGA_CUTOFF: u64 = (1.4 * MEGA) as u64;
+const GIGA_CUTOFF: u64 = (1.4 * GIGA) as u64;
+
+impl SizeDisplay for u64 {
+    fn as_size_display(&self) -> String {
+        if *self < KILO_CUTOFF {
+            return format!("{} B", *self)
+        }
+
+        let (scaled, unit) = match *self {
+            KILO_CUTOFF...MEGA_CUTOFF => (*self as f64 / KILO, "kB"),
+            MEGA_CUTOFF...GIGA_CUTOFF => (*self as f64 / MEGA, "MB"),
+            _ => (*self as f64 / GIGA, "GB"),
+        };
+        format!("{:.2} {}", scaled, unit)
+    }
+}
+
+
