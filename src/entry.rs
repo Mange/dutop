57829f3a0daa99f96bf6f2dd04828ff4290af2f9
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-10-21 13:20:03 +0200

    Add tests for Root & Entry; fix bug in Root
    Bug was that Root assumed that the path was a directory, even though it
    didn't need to. Caused by a refactoring step that wasn't completed.

--- src/entry.rs
+++ src/entry.rs
@@ -7,6 +7,7 @@ use DisplayableEntry;
 use utils;
 use utils::SizeDisplay;
 
+#[derive(Debug)]
 pub struct Entry {
     name: String,
     self_size: u64,
@@ -82,5 +83,62 @@ impl DisplayableEntry for Entry {
 impl fmt::Display for Entry {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {}", self.name(), self.size().as_size_display())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use DisplayableEntry;
+    use std::path::Path;
+
+    #[test]
+    fn it_can_be_constructed_with_a_path() {
+        let pwd = Entry::for_path(Path::new(".")).unwrap();
+
+        assert_eq!(pwd.name(), "./");
+        assert!(pwd.children_iter().count() > 0);
+        assert!(pwd.size() > 0);
+    }
+
+    #[test]
+    fn it_is_error_when_constructed_from_missing_path() {
+        let missing = Entry::for_path(Path::new("./does-not-exist"));
+        assert_eq!(missing.is_err(), true);
+        assert_eq!(missing.unwrap_err(), "File not found");
+    }
+
+    #[test]
+    fn it_is_hidden_when_filename_starts_with_a_dot() {
+        let hidden = Entry::for_path(Path::new("./.gitignore")).unwrap();
+        let normal = Entry::for_path(Path::new("./LICENSE")).unwrap();
+
+        assert_eq!(hidden.is_hidden(), true);
+        assert_eq!(normal.is_hidden(), false);
+    }
+
+    #[test]
+    fn file_entry_has_no_children() {
+        let file = Entry::for_path(Path::new("./LICENSE")).unwrap();
+        assert_eq!(file.children_iter().count(), 0);
+    }
+
+    #[test]
+    fn it_can_be_displayed() {
+        use utils::SizeDisplay;
+
+        let file = Entry::for_path(Path::new("./LICENSE")).unwrap();
+        assert_eq!(format!("{}", file), format!("LICENSE {}", file.size().as_size_display()));
+    }
+
+    #[test]
+    fn it_calculates_size_from_children() {
+        let entry = Entry::for_path(Path::new(".")).unwrap();
+        let children_size = entry
+            .children_iter()
+            .map(|child| child.size())
+            .fold(0, |sum, item| sum + item);
+
+        assert!(entry.size() >= children_size);
     }
 }
--- src/root.rs
+++ src/root.rs
@@ -8,6 +8,7 @@ use entry::Entry;
 use utils;
 use utils::SizeDisplay;
 
+#[derive(Debug)]
 pub struct Root {
     name: String,
     entry: Entry,
@@ -24,7 +25,7 @@ impl Root {
     fn from_metadata(path: &Path, metadata: &fs::Metadata) -> Result<Root, String> {
         Entry::from_metadata(path, &metadata).map(|entry| {
             Root{
-                name: utils::full_name_from_path(path, true),
+                name: utils::full_name_from_path(path, metadata.is_dir()),
                 entry: entry,
             }
         })
@@ -58,5 +59,48 @@ impl DisplayableEntry for Root {
 impl fmt::Display for Root {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {}", self.name(), self.entry.size().as_size_display())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use DisplayableEntry;
+    use std::path::Path;
+
+    #[test]
+    fn it_can_be_constructed_with_a_path() {
+        let pwd = Root::for_path(Path::new(".")).unwrap();
+
+        assert_eq!(pwd.name(), "./");
+        assert_eq!(pwd.is_hidden(), false);
+        assert!(pwd.children_iter().count() > 0);
+        assert!(pwd.size() > 0);
+    }
+
+    #[test]
+    fn it_is_error_when_constructed_from_missing_path() {
+        let missing = Root::for_path(Path::new("./does-not-exist"));
+        assert_eq!(missing.is_err(), true);
+        assert_eq!(missing.unwrap_err(), "File not found");
+    }
+
+    #[test]
+    fn it_can_be_displayed() {
+        use utils::SizeDisplay;
+
+        let root = Root::for_path(Path::new("./LICENSE")).unwrap();
+        assert_eq!(format!("{}", root), format!("./LICENSE {}", root.size().as_size_display()));
+    }
+
+    #[test]
+    fn it_calculates_size_from_children() {
+        let root = Root::for_path(Path::new(".")).unwrap();
+        let children_size = root
+            .children_iter()
+            .map(|child| child.size())
+            .fold(0, |sum, item| sum + item);
+
+        assert!(root.size() >= children_size);
     }
 }
